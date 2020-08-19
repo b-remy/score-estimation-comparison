fead40f4e552e6b96f0426fb59845271c528d2de
b-remy <remy.benjamin.101@gmail.com>
2020-08-19 16:40:16 +0200

    add score viz util

--- utils.py
+++ utils.py
@@ -36,3 +36,24 @@ def get_two_moons(sigma, resolution=1024):
     components_distribution=tfd.MultivariateNormalDiag(loc=coords.T, scale_identity_multiplier=sigma)
     )
   return distribution
+
+
+ def display_score_two_moons(score, distribution, n=28):
+    xc,yc = meshgrid(linspace(-1.5,2.5,204),linspace(-1.,1.5,128))
+    Z = stack([xc.flatten(), yc.flatten()],axis=1).astype('float32')
+    S = distribution.log_prob(Z)
+    figure(dpi=100)
+    imshow(exp(S.reshape((128,204))), cmap='Oranges', origin='lower')
+
+    X = linspace(-1.5,2.5,n)
+    Y = linspace(-1.,1.5,n)
+    points = stack(meshgrid(X, Y), axis=-1).reshape((-1, 2))
+    res, state = score(points, 0.0*jnp.ones((len(points),1)))
+    g = res.reshape([n, n,2])
+
+    #print(g.shape)
+    #g = score(points).reshape([len(Y), len(X), 2])
+    _x = linspace(0, 204, n)
+    _y = linspace(0, 128, n)
+    quiver(_x, _y, g[:,:,0], g[:,:,1])
+    axis('off')

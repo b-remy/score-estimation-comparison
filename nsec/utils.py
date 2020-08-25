3b6a122c81bdddf08871e90fd6c6ad6d0973f03e
b-remy <remy.benjamin.101@gmail.com>
2020-08-25 11:31:46 +0200

    notebook and utils modifications for methods comparison

--- nsec/utils.py
+++ nsec/utils.py
@@ -59,7 +59,7 @@ def display_score_gm(score, distribution, dpi=100, n=28):
     plt.axis('off')
 
 
-def display_score_error_gm(true_score, estimated_scores, labels, distribution=None, n=28, is_amortized=True, is_reg=True, scale=1, offset=[0, 0]):
+def display_score_error_gm(true_score, estimated_scores, labels, distribution=None, n=28, is_amortized=True, is_reg=True, scale=1, offset=[0, 0], is_NF=False):
     scale = scale
     offset = jnp.array(offset)
     c1 = scale * jnp.array([-7., -7]) + offset
@@ -67,14 +67,22 @@ def display_score_error_gm(true_score, estimated_scores, labels, distribution=No
     n = 100
     X = jnp.linspace(c1[0], c2[0], n)
     Y = jnp.linspace(c1[1], c2[1], n)
+
     if distribution:
         _x, _y = jnp.meshgrid(jnp.arange(0, len(X), 1), jnp.arange(0, len(Y), 1))
         Z = jnp.stack(jnp.meshgrid(X, Y), axis=-1).reshape((-1, 2))
         S = distribution.log_prob(Z)
         dist = jnp.exp(S.reshape((len(Y), len(X))))
-    points = jnp.stack(jnp.meshgrid(X, Y), axis=-1).reshape((-1, 2))
-    true_s = true_score(points)
+
+    if is_NF:
+        points = jnp.stack(jnp.meshgrid(X, Y), axis=-1).reshape((-1, 1, 2))
+    else:
+        points = jnp.stack(jnp.meshgrid(X, Y), axis=-1).reshape((-1, 2))
+
+    true_s = true_score(points.reshape((-1, 2)))
+
     true_s = true_s.reshape([len(Y), len(X),2])/jnp.linalg.norm(true_s)
+
     n_s = len(labels)
     estimated_vector_fileds = []
     errors = []
@@ -90,7 +98,9 @@ def display_score_error_gm(true_score, estimated_scores, labels, distribution=No
                 estimated_s, state = score(points)
             else:
                 estimated_s = score(points)
+
         estimated_s = estimated_s.reshape([len(Y), len(X),2])/jnp.linalg.norm(estimated_s)
+
         estimated_vector_fileds.append(estimated_s)
         errors.append(jnp.linalg.norm(estimated_s - true_s, axis=2))
 
@@ -112,7 +122,7 @@ def display_score_error_gm(true_score, estimated_scores, labels, distribution=No
 
     plt.tight_layout()
 
-def display_score_error_two_moons(true_score, estimated_scores, labels, distribution=None, dpi=100, n=28, is_amortized=True, is_reg=True, scale=1, offset=[0, 0]):
+def display_score_error_two_moons(true_score, estimated_scores, labels, distribution=None, dpi=100, n=28, is_amortized=True, is_reg=True, scale=1, offset=[0, 0], is_NF=False):
     scale = scale
     offset = jnp.array(offset)
     d_offset = jnp.array([.5, .25])
@@ -130,30 +140,39 @@ def display_score_error_two_moons(true_score, estimated_scores, labels, distribu
         S = distribution.log_prob(Z)
         dist = jnp.exp(S.reshape((len(Y), len(X))))
 
-    points = jnp.stack(jnp.meshgrid(X, Y), axis=-1).reshape((-1, 2))
-    true_s = true_score(points)
+    if is_NF:
+        points = jnp.stack(jnp.meshgrid(X, Y), axis=-1).reshape((-1, 1, 2))
+    else:
+        points = jnp.stack(jnp.meshgrid(X, Y), axis=-1).reshape((-1, 2))
+
+    true_s = true_score(points.reshape((-1, 2)))
     true_s = true_s.reshape([len(Y), len(X),2])/jnp.linalg.norm(true_s)
     n_s = len(labels)
     estimated_vector_fileds = []
     errors = []
 
-    for score in estimated_scores:
-        if is_amortized:
-            if is_reg:
-                estimated_s, state = score(points, 0.0*jnp.ones((len(points),1)))
-            else:
-                estimated_s = score(points, 0.0*jnp.ones((len(points),1)))
+    for k, score in enumerate(estimated_scores):
+        if k==0 and is_NF:
+            estimated_s = score(points)
+
         else:
-            if is_reg:
-                estimated_s, state = score(points)
+            points = points.reshape((-1, 2))
+            if is_amortized:
+                if is_reg:
+                    estimated_s, state = score(points, 0.0*jnp.ones((len(points),1)))
+                else:
+                    estimated_s = score(points, 0.0*jnp.ones((len(points),1)))
             else:
-                estimated_s = score(points)
+                if is_reg:
+                    estimated_s, state = score(points)
+                else:
+                    estimated_s = score(points)
         estimated_s = estimated_s.reshape([len(Y), len(X),2])/jnp.linalg.norm(estimated_s)
         estimated_vector_fileds.append(estimated_s)
         errors.append(jnp.linalg.norm(estimated_s - true_s, axis=2))
 
-    v_min = jnp.min([jnp.min(e) for e in errors])
-    v_max = jnp.max([jnp.max(e) for e in errors])
+    v_min = jnp.min([jnp.min(e) for e in errors[1:]])
+    v_max = jnp.max([jnp.max(e) for e in errors[1:]])
 
     for i in range(n_s):
         plt.subplot(n_s, 2, 2*i+1)
@@ -164,7 +183,7 @@ def display_score_error_two_moons(true_score, estimated_scores, labels, distribu
             plt.contour(_x, _y, dist, levels=[1], colors='white')
         plt.title('Score error ({}), avg={:.2e}'.format(labels[i], jnp.mean(errors[i])), fontsize=9)
         plt.subplot(n_s, 2, 2*i+2)
-        
+
         g = estimated_vector_fileds[i]
 
         plt.quiver(X[::4], Y[::4], g[::4,::4,0], g[::4,::4,1])

d4be117459594403912455d4904c5ec72abfbb0e
Benjamin Remy <remy.benjamin.101@gmail.com>
2021-10-13 15:29:43 +0200

    change description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='neural-score-estimation-comparison',
       version='0.1',
-      description='Comparison of different methods estimating the score of distributions',
+      description='Solving inverse problems with Denoising Score Matching',
       url='https://github.com/b-remy/score-estimation-comparison',
       author='Benjamin Remy',
       author_email='benjamin.remy@cea.fr',
